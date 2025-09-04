13e79a3fab2786b770c83c93b2a6e74927746b85
ManojNambiraj <postboxmano@gmail.com>
2025-09-04 23:42:50 +0530

    JS Intro

---
+++ JS/Class1/script.js
@@ -0,0 +1,19 @@
+// JS Versions
+
+    // Old    --> ES5
+
+    // Latest --> ES6
+
+// Variables:
+
+    // a = "Good Morning"
+
+    // console.log(a);
+    // console.log(a);
+    // console.log(a);
+    // console.log(a);
+    // console.log(a);
+
+mySalaryAmtInOurCollegeDays = 10
+
+console.log(mySalaryAmtInOurCollegeDays);
