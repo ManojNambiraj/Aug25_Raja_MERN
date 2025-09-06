e4f0d85bfce6a3b6fd6fa7c1005fc0dcf28b0a3d
ManojNambiraj <postboxmano@gmail.com>
2025-09-06 23:18:07 +0530

    JS Datatypes, Hoisting, Operators

---
+++ JS/Class2/script.js
@@ -0,0 +1,94 @@
+// Datatypes:
+
+    // a = null
+
+    // console.log(a);
+    // console.log(typeof(a));
+
+    // Number
+    // String
+    // Boolean
+    // Null
+    // undefined
+
+// Hoisting (or) Variable declaration
+
+    // var --> ES5
+
+        // var a = 10
+
+        // var a = 20
+
+        // console.log(a);
+
+    // let --> ES6
+
+        // let a = 10
+
+        // a = 20
+
+        // console.log(a);
+
+    // const --> ES6
+
+        // const a = 10
+
+        // console.log(a);
+    
+// Versions --> Ecma script
+
+    // ES5
+    // ES6
+
+// Scoping
+
+    // 1. Global Scope --> var
+    // 2. Local Scope --> Let, const
+
+        // {
+        //     const a = 10
+
+        //     console.log("inside -->", a);
+
+        // }
+
+        // console.log("outside -->", a);
+
+// Operators:
+
+    // Arithmetic Operators
+        // (+, -, *, /, %, **, ++, --)
+
+        // let a = 5
+        // let b = 3
+        // let result
+
+        // result = a + b;
+        // result = a - b;
+        // result = a * b;
+        // result = a / b;
+        // result = a ** b;
+        // result = ++a
+        // result = --a
+
+        // console.log(result);        
+
+    // Assignment Operators
+        // (=, +=, -=, *=, /=, %=, **=)
+
+        // let a = 10
+        
+        // a += 5  //  a = a + 5
+        // a -= 5  //  a = a - 5
+        // a *= 5  //  a = a * 5
+        // a /= 5  //  a = a / 5
+        // a %= 5  //  a = a % 5
+        // a **= 5  //  a = a ** 5
+
+        // console.log(a);
+        
+
+    // Comparison Operators
+    // Logical Operators
+    // Ternary Operator
+    // Type Operator
