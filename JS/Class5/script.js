f5fc73e268dfc33638fcf25dc5e942f3bcdaf857
ManojNambiraj <postboxmano@gmail.com>
2025-09-10 23:16:13 +0530

    Arraj and Object

--- JS/Class5/script.js
+++ JS/Class5/script.js
@@ -1,1 +1,55 @@
-// 
+// Array
+
+    // let a = [10, 20, 30, 40, 50]
+    //          0   1   2   3   4 ............
+
+    // console.log(a);
+    // console.log(a[2], a[3]);
+    // // a.push(600)
+    // a.pop()
+
+    // console.log(a);
+
+// Object: {key: value}
+
+    // let student = {
+    //     name: "Ravi",
+    //     age: 22,
+    //     dept: "IT",
+    //     mobile: 876543211,
+    //     address: {
+    //         dno: 2,
+    //         landmark: "abc"
+    //     }
+    // }
+
+    // console.log(student);
+    // console.log(student.age)
+    // console.log(student.dept)
+    // console.log(student.mobile)
+    // console.log(student.address.landmark)
+
+// Destructuring:
+
+    // Array Destructure:
+        // let fruits = ["apple", "banana", "orange", "kiwi"]
+
+        // const [red, yellow, orange, violet] = fruits;
+        
+        // console.log(orange);
+                
+            // let student = {
+            //     name: "Ravi",
+            //     age: 22,
+            //     dept: "IT",
+            //     mobile: 876543211,
+            //     address: {
+            //         dno: 2,
+            //         landmark: "abc"
+            //     }
+            // }
+
+            // const {name, age, dept, mobile} = student
+
+            // console.log(name);
+                        
