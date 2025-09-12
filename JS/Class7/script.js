8b4433d675b2b5486fc8e2397d0de480ccc77a51
ManojNambiraj <postboxmano@gmail.com>
2025-09-12 23:20:28 +0530

    Functions & Call back functions

---
+++ JS/Class7/script.js
@@ -0,0 +1,43 @@
+// Functions
+    // User-defined functions
+        // 1. Normal functions
+        // 2. Ananymous functions
+        // 3. Arrow functions
+        // 4. IFIE functions
+
+// 3. Arrow functions --> ES6
+
+    // const demo = (x) => console.log(x);
+
+    // demo("Hi, I'm");
+
+// 4. IIFE functions:
+    // (Immediately Invoked Functions Expression)
+
+    // (() => {
+    //     console.log("It's IIFE");
+    //     console.log(5*5);
+    // })()
+
+// Callback functions:
+
+    // function diplay(result) {
+    //   console.log(result);
+    // }
+
+    // function sum(a, b, diplay) {
+    //   let result = a + b;
+    //   diplay(result);
+    // }
+
+    // sum(36, 24, diplay);
+
+// Built-in functions
+
+    // setTimeout(() => {
+    //     console.log(10);
+    // }, 4000)
+
+    // setInterval(() => {
+    //     console.log(10);
+    // },3000)
