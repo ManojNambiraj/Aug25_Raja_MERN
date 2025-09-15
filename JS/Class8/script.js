c0bdc9bb1da49f75d06217061d044b8230106d56
ManojNambiraj <postboxmano@gmail.com>
2025-09-15 22:54:54 +0530

    Array methods

---
+++ JS/Class8/script.js
@@ -0,0 +1,26 @@
+// Array Methods:
+    // MRF -> (Map, Reduce, Filter)
+
+// let fruits = ["apple", "banana", "orange", "grapes", "watermelon"]
+
+let nums = [2, 4, 7, 5, 19, 17, 15, 22, 34]
+
+// Map:
+
+    // fruits.map((item) => console.log(item))
+
+    // let result = nums.map((item) => item * 2);
+
+    // console.log(result);
+
+// Filter:
+
+    // let result = nums.filter((item) => item % 2 != 0)
+
+    // console.log(result);
+
+// reduce:
+
+    // let result = nums.reduce((acc, curr) => acc + curr)     
+
+    // console.log(result);
